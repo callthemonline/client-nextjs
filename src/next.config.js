a4e47acfcbde37d47f626ba450be443c9301d869
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-25 22:40:35 +0100

    Add webpack bundle analyzer

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,13 @@
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const withTypescript = require("@zeit/next-typescript");
 const withCss = require("@zeit/next-css");
+
 module.exports = withTypescript(
-  withCss({
-    distDir: "../.next",
-  }),
+  withCss(
+    withBundleAnalyzer({
+      distDir: "../.next",
+      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+    }),
+  ),
 );
