d4e7bb9cfd733ea1bb31fe7328df77156ef10fae
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-26 08:57:18 +0100

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -66,6 +66,26 @@ export default class extends Document {
               "minimum-scale=1, width=device-width, height=device-height"
             }
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-title" content="callthem.online" />
           <meta name="application-name" content="callthem.online" />
