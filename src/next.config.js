693b0e6d9139f1bc85580ecb6b23819312c6da79
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-26 08:22:56 +0100

    Use next-compose-plugins

--- src/next.config.js
+++ src/next.config.js
@@ -1,13 +1,23 @@
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
-const withTypescript = require("@zeit/next-typescript");
 const withCss = require("@zeit/next-css");
+const withTypescript = require("@zeit/next-typescript");
+const withPlugins = require("next-compose-plugins");
 
-module.exports = withTypescript(
-  withCss(
-    withBundleAnalyzer({
-      distDir: "../.next",
-      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-    }),
-  ),
+module.exports = withPlugins(
+  [
+    withTypescript,
+    withCss,
+    [
+      withBundleAnalyzer,
+      {
+        analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ["browser", "both"].includes(
+          process.env.BUNDLE_ANALYZE,
+        ),
+      },
+    ],
+  ],
+  {
+    distDir: "../.next",
+  },
 );
