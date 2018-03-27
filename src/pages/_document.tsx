cd2efd771b83d7bd1a0a0165524c2630b4d607da
Alexander Kachkaev <alexander@kachkaev.ru>
2018-03-27 09:05:06 +0100

    Use internal roboto.css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -94,10 +94,6 @@ export default class extends Document {
             name="theme-color"
             content={pageContext.theme.palette.primary.main}
           />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          />
         </Head>
         <body>
           <Main />
